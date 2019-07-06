59f2616ad243302bd0377cd6beb0aaae5566a3e3
Isabela Baião Dowsley <isabela.dowsley@Isabelas-MacBook-Pro.local>
2019-07-06 17:05:44 +0200

    initial commit to heroku

--- Backend/app.js
+++ Backend/app.js
@@ -51,7 +51,7 @@ app.use(
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
