739de071d38d4ec7ebdffac51d19e565299a74ac
Isabela Baião Dowsley <isabela.dowsley@Isabelas-MacBook-Pro.local>
2019-07-06 18:27:38 +0200

    initial commit to heroku

--- Backend/routes/auth-routes.js
+++ Backend/routes/auth-routes.js
@@ -140,21 +140,4 @@ router.get('/getUser', (req, res, next) => {
 	return;
 });
 
-// Facebook
-
-// passport.use(
-// 	new FacebookStrategy(
-// 		{
-// 			clientID: FACEBOOK_APP_ID,
-// 			clientSecret: FACEBOOK_APP_SECRET,
-// 			callbackURL: 'http://localhost:5000/auth/facebook/callback'
-// 		},
-// 		function(accessToken, refreshToken, profile, cb) {
-// 			User.findOrCreate({ facebookId: profile.id }, function(err, user) {
-// 				return cb(err, user);
-// 			});
-// 		}
-// 	)
-// );
-
 module.exports = router;
