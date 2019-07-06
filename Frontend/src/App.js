50ba3f5b612157a883ddf81de4edb8a3a5183267
Isabela Baião Dowsley <isabela.dowsley@Isabelas-MacBook-Pro.local>
2019-07-06 18:44:28 +0200

    initial commit to heroku

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 	};
 
 	componentDidMount() {
-		fetch(`${process.env.REACT_APP_API_URL}/getUser`, { credentials: 'include' })
+		fetch('https://citycircle.herokuapp.com/getUser', { credentials: 'include' })
 			.then((response) => {
 				console.log(response);
 				if (!response.ok) {
